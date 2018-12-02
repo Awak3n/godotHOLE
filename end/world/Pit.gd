a05a30d262bc18543ffb2ad6d803e14a35f3acc2
Êndril Castilho da Silveira <endril.n@hotmail.com>
2018-12-02 17:36:17 -0200

    Personagens adicionados

--- end/world/Pit.gd
+++ end/world/Pit.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
-signal enemy_fell
+signal golem_fell
+signal skull_fell
+signal ghost_fell
 
 func _ready():
 	connect('body_entered', self, '_on_body_entered')
 
 func _on_body_entered(body):
 	print(body.name)
-	if body.is_in_group('enemies'):
+	if body.is_in_group('Golem'):
 		emit_signal('enemy_fell')
+	elif body.is_in_group('Skull'):
+		emit_signal('skull_fell')
+	elif body.is_in_group('Ghost'):
+		emit_signal('ghost_fell')
 	else:
 		get_tree().reload_current_scene()
--- end/world/Spawner.gd
+++ end/world/Spawner.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-const ENEMY = preload('res://enemies/small/normal/EnemyNormal.tscn')
-const ENEMY_ENRAGED = preload('res://enemies/small/enraged/EnemyEnraged.tscn')
+const GOLEM_ENEMY = preload('res://enemies/Golem/golemSprites/normal/EnemyNormal.tscn')
+const GOLEM_ENRAGED = preload('res://enemies/Golem/golemSprites/enraged/EnemyEnraged.tscn')
+const GHOST_ENEMY = preload('res://enemies/Ghost/ghostSprites/normal/EnemyNormal.tscn')
+const GHOST_ENRAGED = preload('res://enemies/Ghost/ghostSprites/enraged/EnemyEnraged.tscn')
+const SKULL_ENEMY = preload('res://enemies/Skull/skullSprites/normal/EnemyNormal.tscn')
+const SKULL_ENRAGED = preload('res://enemies/Skull/skullSprites/enraged/EnemyEnraged.tscn')
 export var START_DELAY = 5.0
 export var START_MIN_WAIT_TIME = 3.5
 
@@ -13,14 +17,34 @@ func _ready():
 	$Timer.start()
 	
 
-func _on_Pit_enemy_fell():
-	var new_enemy = ENEMY_ENRAGED.instance()
-	new_enemy.connect('died', self, '_on_enemy_died')
+func _on_Pit_golem_fell():
+	var new_enemy = GOLEM_ENRAGED.instance()
+	new_enemy.connect('golem_died', self, '_on_enemy_died')
 	add_child(new_enemy)
 
-func _spawn_enemy():
-	var new_enemy = ENEMY.instance()
-	new_enemy.connect('died', self, '_on_enemy_died')
+func _spawn_golem():
+	var new_enemy = GOLEM_ENEMY.instance()
+	new_enemy.connect('golem_died', self, '_on_enemy_died')
+	add_child(new_enemy)
+
+func _on_Pit_ghost_fell():
+	var new_enemy = GHOST_ENRAGED.instance()
+	new_enemy.connect('ghost_died', self, '_on_enemy_died')
+	add_child(new_enemy)
+
+func _spawn_ghost():
+	var new_enemy = GHOST_ENEMY.instance()
+	new_enemy.connect('ghost_died', self, '_on_enemy_died')
+	add_child(new_enemy)
+
+func _on_Pit_skull_fell():
+	var new_enemy = SKULL_ENRAGED.instance()
+	new_enemy.connect('skull_died', self, '_on_enemy_died')
+	add_child(new_enemy)
+
+func _spawn_skull():
+	var new_enemy = SKULL_ENEMY.instance()
+	new_enemy.connect('skull_died', self, '_on_enemy_died')
 	add_child(new_enemy)
 
 func _set_wait_time():
@@ -29,7 +53,16 @@ func _set_wait_time():
 
 func _on_Timer_timeout():
 	print('timeout')
-	_spawn_enemy()
+	var random = randi()%3
+	if random == 0: 
+		_spawn_ghost()
+		print("Nasceu um fantasma")
+	elif random == 1:
+		_spawn_skull()
+		print("Nasceu uma caveira")
+	else:		
+		_spawn_golem()
+		print("Nasceu um golem")
 	_set_wait_time()
 
 func _on_enemy_died():
